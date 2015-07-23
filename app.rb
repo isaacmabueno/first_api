7db59debbe30d58b311791da84f991a797a788de
isaacmabueno <isaacmabueno@gmail.com>
2015-07-23 14:33:27 -0500

    update

--- app.rb
+++ app.rb
@@ -2,9 +2,8 @@ require 'bundler'
 Bundler.require() #you need to state the bundler.require here and it doesn't need ()
 
 #how to make a router or a controller in 3 lines of code
-#http://localhost/
+# #http://localhost/
 get '/' do
-
   #do something
   {:name => 'Isaac Bueno', :message => 'Hire me, please!'}.to_json
 end
@@ -14,5 +13,14 @@ get'/api/awesome' do
   {:riotfest => 'Thrice is playing Riot Fest', :thrice => 'It\'s going to be sick!'}.to_json
 end
 
+get '/' do
+  erb: index #erb says! hey! go find 'index.erb share it'
+
+end
+
+#get: localhost/api/json
+get 'api/json' do
+  some_json
+end
 
 #rack is a ruby based server that exposes our bundler
