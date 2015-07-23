1ae73b6eca6e5052d1e9a627890b5a34f8db5e80
isaacmabueno <isaacmabueno@gmail.com>
2015-07-23 14:46:31 -0500

    update

--- app.rb
+++ app.rb
@@ -8,19 +8,23 @@ get '/' do
   {:name => 'Isaac Bueno', :message => 'Hire me, please!'}.to_json
 end
 #http://localhost/api/awesome/
-get'/api/awesome' do
+get '/api/awesome' do
   #do something else
   {:riotfest => 'Thrice is playing Riot Fest', :thrice => 'It\'s going to be sick!'}.to_json
 end
 
 get '/' do
-  erb: index #erb says! hey! go find 'index.erb share it'
+  erb :index #erb says! hey! go find 'index.erb share it'
 
 end
 
 #get: localhost/api/json
 get 'api/json' do
   some_json
+end
+
+get '/about' do
+  erb :about
 end
 
 #rack is a ruby based server that exposes our bundler
