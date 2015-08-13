ab4c770ff659211a465b57ae5a6701d29f0a4a3a
isaacmabueno <isaacmabueno@gmail.com>
2015-08-13 11:38:38 -0500

    123

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require 'bundler'
 Bundler.require() #you need to state the bundler.require here and it doesn't need ()
 
-#how to make a router or a controller in 3 lines of code
-# #http://localhost/
+# how to make a router or a controller in 3 lines of code
+#http://localhost/
 get '/' do
   #do something
-  {:name => 'Isaac Bueno', :message => 'Hire me, please!'}.to_json
+  # {:name => 'Isaac Bueno', :message => 'Hire me, please!'}.to_json
+erb :index
 end
 #http://localhost/api/awesome/
 get '/api/awesome' do
@@ -13,14 +14,14 @@ get '/api/awesome' do
   {:riotfest => 'Thrice is playing Riot Fest', :thrice => 'It\'s going to be sick!'}.to_json
 end
 
-get '/' do
-  erb :index #erb says! hey! go find 'index.erb share it'
-
-end
+# get '/' do
+#   erb :index #erb says! hey! go find 'index.erb share it'
+#
+# end
 
 #get: localhost/api/json
-get 'api/json' do
-  some_json
+get '/api/json' do
+  {:message => 'Thrice is playing Riot Fest', :thrice => 'It\'s going to be sick!'}.to_json
 end
 
 get '/about' do
